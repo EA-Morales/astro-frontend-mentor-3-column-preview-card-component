a13d016dc4f088694d161348009ef0d2de0d1b95
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-09-25 22:34:43 -0300

    astro finally working!

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
-    applyBaseStyles: false,
+    applyBaseStyles: true,
   })]
 });
