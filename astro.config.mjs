4c4ada7a6b8f42cdd5de0c755684a88692b38e7a
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-10-26 17:23:58 -0300

    adding base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ea-morales.github.io',
+  base: '/astro-frontend-mentor-3-column-preview-card-component',
   integrations: [tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
