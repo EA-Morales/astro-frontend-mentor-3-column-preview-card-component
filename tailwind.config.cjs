bcdcf39a8b9c80b54c53983d71b39ddd37e6716c
l3gol4s <eduardo.agustin.morales@gmail.com>
2023-10-25 22:45:47 -0300

    almost finish

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        cursive: ['Big Shoulders Display', 'cursive'],
+        sans: ['Lexend Deca', 'sans-serif'],
+      },
+      colors: {
+        'Bright-orange': 'hsl(31, 77%, 52%)',
+        'Dark-cyan': 'hsl(184, 100%, 22%)',
+        'Very-dark-cyan': 'hsl(179, 100%, 13%)',
+        'Transparent-white': 'hsla(0, 0%, 100%, 0.75)',
+        'Very-light-gray': 'hsl(0, 0%, 95%)',
+      },
+    },
+  },
+  plugins: [],
+};
